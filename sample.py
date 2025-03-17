875ad9dd6676f433340a0099a9e1b85ab665ac0b
ranasidhu91-debug <60548991+ranasidhu91-debug@users.noreply.github.com>
2025-03-18 00:04:33 +0800

    it worked

--- sample.py
+++ sample.py
@@ -3,9 +3,9 @@ from API.api_key import iqair_key,open_weather
 from geopy.geocoders import Nominatim
 from pprint import pprint
 
-lat = str(6.443589)
+lat = str(1.5646)
 lng = str(101.435110)
-lon = str(100.216599)
+lon = str(104.2451)
 
 #aqi
 #aqi_url_complete = f"http://api.waqi.info/feed/geo:{lat};{lng}/?token={waqi_key}" # aqi api
@@ -14,24 +14,22 @@ lon = str(100.216599)
 #aqi_url_complete = f"https://api.waqi.info/feed/klang/?token=8437227b3dd6cba3e470d7a48fabb84f26c448ec"
 #print(aqi_url_complete)
 #openweather 
-aqi_url_complete = f"http://api.airvisual.com/v2/city?city=Kuala Lumpuer&state=California&country=USA&key={{YOUR_API_KEY}}" #open weather api
-
-#whatever = f"http://api.openweathermap.org/data/2.5/air_pollution/history?lat={lat}&lon={lon}&start=1742132952&end=1742219352&appid={open_weather}"
+whatever = f"http://api.openweathermap.org/data/2.5/air_pollution?lat={lat}&lon={lon}&appid={open_weather}"
 
 
-#response = requests.get(aqi_url_complete)
-
-#data = response.json()
-
-#pprint(data)
-
-
-
-
-city_data = f'http://api.airvisual.com/v2/city?city=Bandar Penawar&state=Johor&country=Malaysia&key={iqair_key}'
-
-response = requests.get(city_data)
+response = requests.get(whatever)
 
 data = response.json()
 
 pprint(data)
+
+
+
+
+#city_data = f'http://api.airvisual.com/v2/city?city=Bandar Penawar&state=Johor&country=Malaysia&key={iqair_key}'
+
+#response = requests.get(city_data)
+
+#data = response.json()
+
+#pprint(data)
